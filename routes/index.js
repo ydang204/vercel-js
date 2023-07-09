f773331ac61bd584797f685c6abc943cac9ab477
Y Dang <daty.danghuynh20497@gmail.com>
2023-07-09 18:04:23 +0700

    remove code

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,10 @@
 const { Router } = require('express');
 const { SuccessResponseObject } = require('../common/http');
 const demo = require('./demo.route');
-const productRoutes = require("./product");
-const authRoutes = require("./auth");
-const { verifyToken } = require("../validation");
 
 const r = Router();
 
 r.use('/demo', demo);
-// r.use("/api/products", verifyToken, productRoutes);
-// r.use("/api/user", authRoutes);
 
 r.get('/', (req, res) => res.json(new SuccessResponseObject('express vercel boiler plate')));
 
