4668d80e08bd38c78dc885d642289eef582ab185
Y Dang <daty.danghuynh20497@gmail.com>
2023-07-09 18:07:26 +0700

    Revert "remove code"
    This reverts commit f773331ac61bd584797f685c6abc943cac9ab477.

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const helmet = require('helmet');
 const { ErrorResponseObject } = require('./common/http');
 const routes = require('./routes');
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const app = express();
 
@@ -9,6 +11,22 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use(helmet());
 app.use('/', routes);
+//parse request of content type JSON
+// app.use(bodyParser.json());
+
+// require("dotenv-flow").config();
+
+// //connect mongodb
+// mongoose.connect
+// (
+//     process.env.DBHOST,
+//     {
+//         useUnifiedTopology: true,
+//         useNewUrlParser: true
+//     }
+// ).catch(error => console.log("Error connecting to mongoDB:" + error));
+
+// mongoose.connection.once("open", () => console.log("Connected successfully to mongoDB"));
 
 // default catch all handler
 app.all('*', (req, res) => res.status(404).json(new ErrorResponseObject('route not defined')));
